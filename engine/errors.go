4630bd009b63d80bdea2be64b0259b217e011c3b
Michael Coyne <mjc@hey.com>
2026-05-25 11:33:03 -0400

    Remove crawler from engine

--- engine/errors.go
+++ engine/errors.go
@@ -2,6 +2,4 @@ package engine
 
 import "errors"
 
-var ErrNoCrawlerVisit = errors.New("no visit from crawler")
-var ErrInvalidCrawlerState = errors.New("crawler has no current visit")
 var ErrUnknownAction = errors.New("unknown action")
